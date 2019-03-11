73272ef1b654c31bf8cee644bd51491a0a8ff552
Roland Gabriel Molina <rolandgnm@gmail.com>
2019-03-11 19:09:59 +1100

    Updates

---
+++ precache-manifest.5869d04b6847befad66af05c1547ff54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d76f1fbb74b44cd077b6",
+    "url": "/xyz-clothing/static/css/main.0495c808.chunk.css"
+  },
+  {
+    "revision": "d76f1fbb74b44cd077b6",
+    "url": "/xyz-clothing/static/js/main.0df06420.chunk.js"
+  },
+  {
+    "revision": "0e837e9f64f41ef1f182",
+    "url": "/xyz-clothing/static/js/runtime~main.19d6fdc0.js"
+  },
+  {
+    "revision": "d9b0d782d20728a40977",
+    "url": "/xyz-clothing/static/js/2.1dbc91e8.chunk.js"
+  },
+  {
+    "revision": "04d5805e0e25f02985db095ea45d6ffd",
+    "url": "/xyz-clothing/index.html"
+  }
+];
