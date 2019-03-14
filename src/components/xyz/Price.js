e4eb6b12dcff7364e84c5976b2ebd6dbd914752c
Roland Gabriel Molina <rolandgnm@gmail.com>
2019-03-14 16:41:31 +1100

    adds large mode to Price

--- src/components/xyz/Price.js
+++ src/components/xyz/Price.js
@@ -13,17 +13,17 @@ const getDisplayPrice = (amount, exchangeRates, fromBase, toBase) => {
 
 const PriceWrapper = styled.span`
   color: ${COLOR.TEXT};
-  font-size: 1.2em;
+  font-size: ${({ large }) => (large ? '2.5em' : '1.2em')};
 
   & sup {
     font-size: 0.6em;
   }
 `;
 
-const Price = ({ base: productBase, amount }) => {
+const Price = ({ base: productBase, amount, large }) => {
   const { currency: selectedCurrency, exchangeRates } = useContext(CurrencyContext);
   return (
-    <PriceWrapper>
+    <PriceWrapper large={large}>
       <sup>{selectedCurrency}</sup>
       {getDisplayPrice(amount, exchangeRates, productBase, selectedCurrency)}
     </PriceWrapper>
