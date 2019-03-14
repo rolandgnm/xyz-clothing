596bbb34c529a07fbba159bb29127f05c908e0a0
Roland Gabriel Molina <rolandgnm@gmail.com>
2019-03-14 18:36:35 +1100

    remove unneeded files

--- src/components/Button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import { ButtonWrapper } from './styled';
-
-export const Button = ({ children, ...props }) => (
-  <ButtonWrapper type="button" {...props}>
-    {children}
-  </ButtonWrapper>
-);
