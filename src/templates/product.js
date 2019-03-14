16bc2f6d15cad75cbb43d1f8b4b3dfe1d697f61b
Roland Gabriel Molina <rolandgnm@gmail.com>
2019-03-14 16:46:38 +1100

    Add some styles to product page

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'gatsby';
 import { Button, Icon } from 'semantic-ui-react';
 import ProductSummary from '../components/xyz/ProductSummary';
@@ -12,6 +13,25 @@ const getSelectedProduct = (products, selectedId) =>
 const getRelatedProducts = (products, relatedProducts) =>
   products ? products.filter(prod => relatedProducts.includes(prod.id)) : [];
 
+const Id = styled.span`
+  color: ${COLOR.TEXT_WEAK};
+  font-size: 0.5em;
+`;
+
+const Description = styled.p`
+  margin-bottom: 2em;
+`;
+
+const DetailsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const NameDescWrapper = styled.div`
+  flex: 1 0 500px;
+  margin: 0 1em 2em 0;
+`;
+
 const ProductPage = ({ pageContext: { products, selectedId } }) => {
   const selectedProduct = getSelectedProduct(products, selectedId);
 
@@ -19,25 +39,22 @@ const ProductPage = ({ pageContext: { products, selectedId } }) => {
   const { id, name, description, price } = selectedProduct;
   return (
     <Layout>
-      <div>
-        <Link to="/" style={{ color: COLOR.TEXT }}>
-          <Button icon circular>
-            <Icon name="arrow left" />
-          </Button>
-          Products
-        </Link>
-      </div>
-      <br />
-      <div>
-        <h2>
-          #{id} {name}
-        </h2>
-        <p>{description}</p>
-        <div>
-          {' '}
-          <Price {...price} />{' '}
-        </div>
-      </div>
+      <Link to="/" style={{ color: COLOR.TEXT }}>
+        <Button icon circular>
+          <Icon name="arrow left" />
+        </Button>
+        Products
+      </Link>
+      <DetailsWrapper>
+        <NameDescWrapper>
+          <h2>
+            {name}
+            <Id> #{id}</Id>
+          </h2>
+          <Description>{description}</Description>
+        </NameDescWrapper>
+        <Price {...price} large />
+      </DetailsWrapper>
       <br />
       <h3>Related</h3>
       <div>
